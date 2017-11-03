8d648bf2f9331ba4368e0b5b2bc11df6ed4ce825
Varun <varunm22@gmail.com>
2017-11-02 23:27:07 -0400

    made data input include validation and test:

--- data_input.py
+++ data_input.py
@@ -5,27 +5,33 @@ import os
 IMG_SIZE = 1024 # num pixels in image
 IMG_DIM = (32, 32) 
 
-# set num to the number of examples you want per image. Default is all
-def import_train(num = None):
+# training data has 1700 examples per char. We're setting 200 aside for validation
+# set param num to the number of examples you want per image, default/max is 1500
+def import_train(num = 1500):
+  return import_data(0, min(num, 1500), "./Data/Train")
+
+# same meaning for param num, default/max is 200
+def import_valid(num = 200):
+  return import_data(1500, min(num, 200), "./Data/Train")
+
+# same meaning for param num, default/max is 300
+def import_test(num = 300):
+  return import_data(0, min(num, 300), "./Data/Test")
+
+def import_data(start, num, path):
   chars = {}
-  n = 0
-  dirs = [x[0] for x in os.walk("./Train")][1:]
+  dirs = [x[0] for x in os.walk(path)][1:]
   for d in dirs:
     if d[8] == 'c':
       c = d.split('_', 1)[1]
     else:
       c = d.split('/')[2]
-    imgs = os.listdir(d)
-    if num is None:
-      chars[c] = np.zeros((len(imgs), IMG_SIZE))
-    else:
-      chars[c] = np.zeros((num, IMG_SIZE))
-      imgs = imgs[:num]
-    n += len(imgs)
+    imgs = os.listdir(d)[start:start+num]
+    chars[c] = np.zeros((num, IMG_SIZE))
     for i in range(len(imgs)):
       chars[c][i,] = np.array(Image.open(d + '/' + imgs[i]).convert('L')).ravel()
 
-  X = np.zeros((n, IMG_SIZE))
+  X = np.zeros((num*len(dirs), IMG_SIZE))
   Y = []
   count = 0
   for char in chars:
@@ -36,3 +42,5 @@ def import_train(num = None):
 
   return (X, Y)
 
+X, Y = import_train()
+print X.shape, Y.shape
